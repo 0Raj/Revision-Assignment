ce8d297bd2802a5b98e6151ee4ee7ddafb746336
0Raj <rajasekark0@gmail.com>
2022-09-19 09:24:11 +0530

    Day3 completed

--- untitled1/Day3/Problem2.java
+++ untitled1/Day3/Problem2.java
@@ -1,4 +1,39 @@
 package Day3;
 
 public class Problem2 {
+   /*   Part 1 : Check Whether the below code will execute or not. Give Reason with your answer
+                 Code will execute because it is possible to extend interface from another interface.
+
+        Part 2 : Check Whether the below code will execute or not. Give Reason with your answer
+                 In the interface it is not possible to use the "implements" key word because
+                 we can extend interface from another but cannot implement it.
+
+        Part 3 : Check Whether the below code will execute or not. Give Reason with your answer
+                 In the interface Implementation it is not possible to use the "implements" key word because
+                  we can extend interface from another  but cannot implement it.
+
+        Part 4 : Check Whether the below code will execute or not. Give Reason with your answer
+                 In the abstract class ImplementationAbstract it is possible to provide implements to interface
+                 it's not be instantiated needs to extended by child class to create the object of that.
+
+        Part 5 : Check Whether the below code will execute or not. Give Reason with your answer
+                 In the above code the variable needs to be initiated because in the interface the variable
+                 is by default public static final .
+
+        Part 6 : Check Whether the below code will execute or not. Give Reason with your answer
+                 In the above code will run because in the interface the variable
+                 is by default public static final .
+
+        Part 7 : Check Whether the below code will execute or not. Give Reason with your answer
+                 From java 8 the default methods are introduce int the interfaces to give some extra flexibility and
+                 functionality to the interface and will have the body.
+
+        Part 8 : Check Whether the below code will execute or not. Give Reason with your answer
+                 In the above code we get the output as "default" because test will have Implementation.
+
+        Part 9 : Check Whether the below code will execute or not. Give Reason with your answer
+                 In the above code we get the output as "override" because test will have Implementation
+                 but gives the priority to the implemented method.
+
+    */
 }
